c20efd8f4c4e5fca504748e80aa71696eedb90ed
inoygods <psihoanalitikinspector@gmail.com>
2024-11-21 04:53:19 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Переключение меню
-document.getElementById('menuBtn').onclick = function() {
-    var dropdown = document.getElementById('menuDropdown');
-    dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
-};
